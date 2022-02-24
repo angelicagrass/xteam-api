014d2b0a7a52ad533c129ff85332a5234380c50d
angelicagrass <angelicagrass@hotmail.com>
2022-02-24 10:07:51 +0100

    router

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import controller from '../controllers/user';
+import extractJWT from '../middleware/extractJWT';
 
 export const router = express.Router();
 
-router.get('/validate', controller.validateToken);
+router.get('/validate',extractJWT, controller.validateToken);
 router.post('/register', controller.registerUser);
 router.post('/login', controller.loginUser);
 router.get('/get/all', controller.getAllUsers);
